0a0ea7ff58f9f847c7363687839d51de3e9df1e0
Nick <i@httpd.cc>
2022-08-22 09:41:45 +0800

    perf: 优化基础请求地址

--- src/Kernel/BaseApi.php
+++ src/Kernel/BaseApi.php
@@ -4,5 +4,11 @@ namespace Cblink\Service\IDaas\Kernel;
 
 class BaseApi extends \Cblink\Service\Foundation\BaseApi
 {
-
+    /**
+     * @return string
+     */
+    public function getBaseUrl()
+    {
+        return $this->app->config['auth_url'];
+    }
 }
