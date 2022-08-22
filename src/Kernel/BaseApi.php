5391316b0ba3ebe2c295f53e6502aa1f44b2e22d
Nick <i@httpd.cc>
2022-08-22 10:05:20 +0800

    fix: 移除修复内容

--- src/Kernel/BaseApi.php
+++ src/Kernel/BaseApi.php
@@ -4,11 +4,4 @@ namespace Cblink\Service\IDaas\Kernel;
 
 class BaseApi extends \Cblink\Service\Foundation\BaseApi
 {
-    /**
-     * @return string
-     */
-    public function getBaseUrl()
-    {
-        return $this->app->config['auth_url'];
-    }
 }
